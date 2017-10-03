4f9d017ff55a29b85968888aea9359aec6413443
Johnathon Bliss <jbliss9821@gmail.com>
2017-10-03 12:40:01 -0500

    Working on backend styelsheet

--- customerBackend.php
+++ customerBackend.php
@@ -9,15 +9,17 @@
 	$shoe_total = $shoe_count *45;
 	$car_total = $car_count * 2500000;
 	$sale_total = $hat_total + $shoe_total + $car_total + $ship;
-
-	echo 'Username: '.$user.'<br>';
-	echo 'Password: '.$pass.'<br><br>';
-	echo 'Shipping Fees: $'.$ship.'<br>';
+	echo '<link href="style.css" rel="stylesheet" type="text/css"/>';
+	echo '<html><head><h1>Receipt for Sale</h1></head><body><br>';
+	echo '<div class="user_back">Username: '.$user.'<br>';
+	echo 'Password: '.$pass.'</div><br>';
+	echo '<div class="receipt">Shipping Fees: $'.$ship.'<br>';
 	echo 'Hats: '.$hat_count.' hat(s) at $20/each = $';
 	echo $hat_total.'<br>';
 	echo 'Shoes: '.$shoe_count.' pair(s) of shoes at $45/pair = $';
 	echo $shoe_total.'<br>';
 	echo 'Cars: '.$car_count.' car(s) at $2.5 million/car = $';
-	echo $car_total.'<br><br>';
-	echo 'Total Sale including shipping: $'.$sale_total.'<br>';
+	echo $car_total.'</div><br>';
+	echo '<div class="total">Total Sale including shipping: $'.$sale_total.'<br></div>';
+	echo '</body></html>'
 ?>
